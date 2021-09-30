8fe74659606bcc99b5848abac0dd5383ad755c5b
anima-libera <anim.libera@gmail.com>
2021-09-30 17:02:22 +0200

    Added placeholders

--- build.py
+++ build.py
@@ -5,30 +5,49 @@ class Page:
 	def __init__(self, **kwargs):
 		self.__dict__.update(kwargs)
 
+def tag_regex(tag_name, property_name):
+	return r"<\s*{tag}\s+{property}\s*=\s*\"([^\"]*)\"\s*/>".format(
+		tag = tag_name, property = property_name)
+
+def handle_insert_tags(code, page):
+	def repl(match):
+		file_path = f"src/{match.group(1)}"
+		print(f"Inserting {file_path}")
+		return open(file_path, "r").read()
+	return re.sub(tag_regex("insert", "file"), repl, code)
+
+def handle_placeholder_tags(code, page):
+	def repl(match):
+		what = match.group(1)
+		print(f"Filling placeholder {what}")
+		return page.__dict__[what]
+	return re.sub(tag_regex("placeholder", "what"), repl, code)
+
+transf_all = [
+	handle_insert_tags,
+	handle_placeholder_tags,
+]
+
 page_list = [
 	Page(
 		name = "Home Page",
 		path_src = "src/home.html",
 		path_gen = "gen/home.html",
+		transf_list = transf_all,
 	),
 	Page(
 		name = "Tabs Page",
 		path_src = "src/tabs.html",
 		path_gen = "gen/tabs.html",
+		transf_list = transf_all,
 	),
-]
-
-def handle_insert_tags(code):
-	def replace_insert(match):
-		file = open(f"src/{match.group(1)}", "r")
-		content = file.read()
-		file.close()
-		return content
-	return re.sub(r"<\s*insert\s+file\s*=\s*\"([^\"]*)\"\s*/>",
-		replace_insert, code)
-
-transformation_list = [
-	handle_insert_tags,
+	Page(
+		name = "Test Page",
+		path_src = "src/template.html",
+		path_gen = "gen/test.html",
+		transf_list = transf_all,
+		content = "awa ^^",
+	),
 ]
 
 for page in page_list:
@@ -38,9 +57,9 @@ for page in page_list:
 	src_file.close()
 
 	gen_code = src_code
-	for transformation in transformation_list:
+	for transformation in page.transf_list:
 		print(f"Applying {transformation.__name__}")
-		gen_code = transformation(gen_code)
+		gen_code = transformation(gen_code, page)
 
 	gen_file = open(page.path_gen, "w")
 	gen_file.write(gen_code)
